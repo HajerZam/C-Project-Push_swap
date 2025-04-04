a6f539b79160a66fcd57567896d0b08ae5d806ad
Meiphim_ <hajer.zmazi2004@gmail.com>
2025-04-04 17:43:04 +0200

    Update sort_utils.c

--- src/sort_utils.c
+++ src/sort_utils.c
@@ -57,6 +57,17 @@ void	move_min_to_top(t_stack *a, int min_index)
 			rra(a);
 	}
 }
+void	skip_sign(const char *str, int *i, int *sign)
+{
+	while (str[*i] && (str[*i] == ' ' || (str[*i] >= '\t' && str[*i] <= '\r')))
+		(*i)++;
+	if (str[*i] == '-' || str[*i] == '+')
+	{
+		if (str[*i] == '-')
+			*sign = -1;
+		(*i)++;
+	}
+}
 
 long	ft_atol(const char *str)
 {
@@ -67,16 +78,16 @@ long	ft_atol(const char *str)
 	i = 0;
 	sign = 1;
 	res = 0;
-	while (str[i] && (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r')))
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
+	skip_sign(str, &i, &sign);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
+		if (res > (2147483647 - (str[i] - '0')) / 10)
+		{
+			if (sign == 1)
+				return (2147483648);
+			else
+				return (-2147483649);
+		}
 		res = res * 10 + (str[i] - '0');
 		i++;
 	}
